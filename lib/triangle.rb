eaddbf936663914096569de5ac180fe89cb7c4b2
Vincent Tran <vincentuong@yahoo.com>
2021-06-30 01:25:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
       raise TriangleError
     elsif @side1 == @side2 && @side1 == @side3
       :equilateral
-    elsif (@side1 == @side2 || @side2 == @)
+    elsif (@side1 == @side2 || @side2 == @side3 || @side3 == @side1)
       :isosceles
     elsif @side1 != @side2 && @side2 != @side3 && @side1 != @side3
       :scalene
